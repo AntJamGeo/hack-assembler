c063f68d674979f96c17d59afa5136e7660feb47
ag <>
2022-12-11 03:55:44 +0000

    Provide functionality for catching invalid A-Instructions

--- asmtools/asmparser.py
+++ asmtools/asmparser.py
@@ -4,25 +4,28 @@ class Parser():
     
     def parse(self, line):
         if len(line) < 1: # ignore whitespace
-            return ""
+            return False
         elif line[0] == "@": # A-Instruction
             self.line_no += 1
             return self._a_inst(line)
         elif line[0] == "(": # L-Instruction
-            return ""
-        elif line[0] in ["A", "D", "M", "0", "1", "-", "!"]: # C-Instruction
+            return False
+        elif line[0] in {"A", "D", "M", "0", "1", "-", "!"}: # C-Instruction
             self.line_no += 1
             return self._c_inst(line)
         elif len(line) > 1 and line[:2] == "//": # ignore comments
-            return ""
+            return False
         else:
-            raise InstructionError(f"Invalid Instruction: {line}")
+            raise InvalidInstructionError(line)
 
     def _a_inst(self, line):
-        for i in range(1, len(line)):
-            if line[i] == " ":
-                break
-        return ("A", line[1:i+1])
+        comment_index = line.find("//")
+        if comment_index != -1:
+            line = line[:comment_index].strip()
+        if len(line) > 1 and len(line.split()) == 1:
+            return ("A", line[1:])
+        else:
+            raise InvalidInstructionError(line)
 
     def _l_inst(self, line):
         return ""
@@ -30,5 +33,5 @@ class Parser():
     def _c_inst(self, line):
         return ("C", "", "", "")
 
-class InstructionError(Exception):
+class InvalidInstructionError(Exception):
     pass
