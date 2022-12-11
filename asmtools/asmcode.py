0f0767407fd52523494ae1bfe044e34fbc72a2a3
ag <>
2022-12-11 15:55:04 +0000

    Provide ability to translate C-Instructions

--- asmtools/asmcode.py
+++ asmtools/asmcode.py
@@ -1,15 +1,24 @@
-def code(instruction):
-    if instruction[0] == "A":
-        bits = bin(int(instruction[1])).replace("0b", "")
+def code(inst):
+    if inst[0] == "A":
+        bits = bin(int(inst[1])).replace("0b", "")
         return "0" * (16 - len(bits)) + bits
-    if instruction[0] == "C":
-        return "111" + dest(instruction[1]) + comp(instruction[2]) + jump(instruction[3])
+    if inst[0] == "C":
+        return "111" + _dest[inst[1]] + _comp[inst[2]] + _jump[inst[3]]
 
-def dest(mnemonic):
-    return ""
+_dest = {None: "000", "M": "001", "D": "010", "MD": "011", "A": "100",
+         "AM": "101", "AD": "110", "AMD": "111"}
 
-def comp(mnemonic):
-    return ""
+_comp = {"0": "0101010", "1": "0111111", "-1": "0111010",
+         "D": "0001100", "A": "0110000", "M": "1110000",
+         "!D": "0001101", "!A": "0110001", "!M": "1110001",
+         "-D": "0001111", "-A": "0110011", "-M": "1110011",
+         "D+1": "0011111", "A+1": "0110111", "M+1": "1110111",
+         "D-1": "0001110", "A-1": "0110010", "M-1": "1110010",
+         "D+A": "0000010", "D+M": "1000010",
+         "D-A": "0010011", "D-M": "1010011",
+         "A-D": "0000111", "M-D": "1000111",
+         "D&A": "0000000", "D&M": "1000000",
+         "D|A": "0010101", "D|M": "1010101"}
 
-def jump(mnemonic):
-    return ""
+_jump = {None: "000", "JGT": "001", "JEQ": "010", "JGE": "011", "JLT": "100",
+         "JNE": "101", "JLE": "110", "JMP": "111"}
