e86c13e3d04c2ab3e683cd9a2c43899982f95b99
ag <>
2022-12-11 21:13:02 +0000

    Reformat asmcode.py to allow for symbol table

--- asmtools/asmcode.py
+++ asmtools/asmcode.py
@@ -1,17 +1,24 @@
 from .asmexceptions import DestinationError, ComputationError, JumpError 
 
-def code(inst):
-    if inst[0] == "A":
-        bits = bin(int(inst[1])).replace("0b", "")
-        return "0" * (16 - len(bits)) + bits
-    if inst[0] == "C":
-        if inst[1] not in _dest:
-            raise DestinationError(inst[4], inst[5], inst[1])
-        if inst[2] not in _comp:
-            raise ComputationError(inst[4], inst[5], inst[2])
-        if inst[3] not in _jump:
-            raise JumpError(inst[4], inst[5], inst[3])
-        return "111" + _comp[inst[2]] + _dest[inst[1]] + _jump[inst[3]]
+class Encoder():
+    def __init__(self):
+        self.table = {"SP": 0, "LCL": 1, "ARG": 2, "THIS": 3, "THAT": 4,
+                      "SCREEN": 16384, "KBD": 24576}
+        for i in range(16):
+            self.table["R"+str(i)] = str(i)
+
+    def code(self, inst):
+        if inst[0] == "A":
+            bits = bin(int(inst[1])).replace("0b", "")
+            return "0" * (16 - len(bits)) + bits
+        if inst[0] == "C":
+            if inst[1] not in _dest:
+                raise DestinationError(inst[4], inst[5], inst[1])
+            if inst[2] not in _comp:
+                raise ComputationError(inst[4], inst[5], inst[2])
+            if inst[3] not in _jump:
+                raise JumpError(inst[4], inst[5], inst[3])
+            return "111" + _comp[inst[2]] + _dest[inst[1]] + _jump[inst[3]]
 
 _dest = {None: "000", "M": "001", "D": "010", "MD": "011", "A": "100",
          "AM": "101", "AD": "110", "AMD": "111"}
--- assembler.py
+++ assembler.py
@@ -1,13 +1,14 @@
 import sys
-from asmtools import Parser, code
+from asmtools import Parser, Encoder
 
 if __name__ == "__main__":
     file_name = sys.argv[1]
     new_file_name = sys.argv[1][:-3] + "hack"
     p = Parser()
+    e = Encoder()
     with open(sys.argv[1], 'r') as in_file, open(new_file_name, 'w') as out_file:
         for line in in_file:
             instruction = p.parse(line.strip())
             if instruction:
-                bits = code(instruction)
+                bits = e.code(instruction)
                 out_file.write(bits + "\n")
