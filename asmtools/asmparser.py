1817b6790eaf957b7898b0a50e405696f096ea70
ag <>
2022-12-12 01:22:42 +0000

    Clean up asmparser.py

--- asmtools/asmparser.py
+++ asmtools/asmparser.py
@@ -4,51 +4,53 @@ class Parser():
     def __init__(self):
         self.line_no = 0
         self.total_lines = 0
+        self.instruction = None
 
-    def parse(self, line):
+    def parse(self, instruction):
+        self.instruction = instruction
         self.total_lines += 1
         """Determine which type of instruction is being given."""
-        if len(line) < 1: # ignore whitespace
+        if len(self.instruction) < 1: # ignore whitespace
             return False
-        elif line[0] == "@": # A-Instruction
+        elif self.instruction[0] == "@": # A-Instruction
             self.line_no += 1
-            return self._a_inst(line)
-        elif line[0] == "(": # L-Instruction
+            return self._a_inst()
+        elif self.instruction[0] == "(": # L-Instruction
             return False
-        elif line[0] in {"A", "D", "M", "0", "1", "-", "!"}: # C-Instruction
+        elif self.instruction[0] in {"A", "D", "M", "0", "1", "-", "!"}: # C-Instruction
             self.line_no += 1
-            return self._c_inst(line)
-        elif len(line) > 1 and line[:2] == "//": # ignore comments
+            return self._c_inst()
+        elif len(self.instruction) > 1 and self.instruction[:2] == "//": # ignore comments
             return False
-        raise InstructionError(self.total_lines, line)
+        raise InstructionError(self.total_lines, self.instruction)
 
 
-    def _a_inst(self, line):
+    def _a_inst(self):
         """Parse A-Instructions."""
-        stripline = _strip_comments(line)
-        if stripline == "@":
-            raise NoAddressError(self.total_lines, line)
-        if len(stripline.split()) > 1:
-            raise BadAddressError(self.total_lines, line, stripline[1:]) 
-        return ("A", stripline[1:])
+        inst = _strip_comments(self.instruction)
+        if inst == "@":
+            raise NoAddressError(self.total_lines, self.instruction)
+        if len(inst.split()) > 1:
+            raise BadAddressError(self.total_lines, self.instruction, inst[1:]) 
+        return ("A", inst[1:])
 
-    def _l_inst(self, line):
+    def _l_inst(self):
         """Parse L-Instructions."""
         return ""
 
-    def _c_inst(self, line):
+    def _c_inst(self):
         """Parse C-Instructions."""
-        stripline = _strip_comments(line) # remove comments
+        inst = _strip_comments(self.instruction) # remove comments
 
         # Find first '=' and split into dest and comp;jump parts
-        if stripline[-1] == "=": # no final '=' simplifies later calculation
-            raise InstructionError(self.total_lines, line)
-        equals_index = stripline.find("=")
+        if inst[-1] == "=": # no final '=' simplifies later calculation
+            raise InstructionError(self.total_lines, self.instruction)
+        equals_index = inst.find("=")
         if equals_index == -1:
-            dest, compjump = None, stripline
+            dest, compjump = None, inst
         else:
-            dest = stripline[:equals_index]
-            compjump = stripline[equals_index+1:] # no final '=' so this is allowed
+            dest = inst[:equals_index]
+            compjump = inst[equals_index+1:] # no final '=' so this is allowed
 
         # Find first ';' and split into comp and jump parts
         semi_colon_index = compjump.find(";")
@@ -58,7 +60,7 @@ class Parser():
             comp = compjump[:semi_colon_index]
             jump = compjump[semi_colon_index+1:]
 
-        return ("C", dest, comp, jump, self.total_lines, line) 
+        return ("C", dest, comp, jump, self.total_lines, self.instruction) 
 
 def _strip_comments(line):
     """Remove any comments from a line."""
